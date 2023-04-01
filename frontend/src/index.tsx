24f504caac50832cf7e0508764a648ebfafa7d42
ant52ho <87661562+ant52ho@users.noreply.github.com>
2023-04-01 02:16:25 -0400

    react routing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/helloworld",
+    element: <div>Hello World</div>,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <RouterProvider router={router} />
+
   </React.StrictMode>
 );
 
