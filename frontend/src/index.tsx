70947531fd53842b5dcc79a6b08c6d994713edf6
ant52ho <87661562+ant52ho@users.noreply.github.com>
2023-04-01 12:11:02 -0400

    routing + portfolio templating

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,33 +2,98 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import ResultPage from './result_site/src/App'
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+
+// Site imports
+import { AnimatePresence } from 'framer-motion';
+import { lazy, Suspense } from 'react';
+import ScrollToTop from './result_site/src/components/ScrollToTop';
+import AppFooter from './result_site/src/components/shared/AppFooter';
+import AppHeader from './result_site/src/components/shared/AppHeader';
+import './result_site/src/css/App.css';
+import UseScrollToTop from './result_site/src/hooks/useScrollToTop';
+import './result_site/src/css/main.css';
+
+const About = lazy(() => import('./result_site/src/pages/AboutMe'));
+const Contact = lazy(() => import('./result_site/src/pages/Contact.jsx'));
+const Home = lazy(() => import('./result_site/src/pages/Home'));
+const Projects = lazy(() => import('./result_site/src/pages/Projects'));
+const ProjectSingle = lazy(() => import('./result_site/src/pages/ProjectSingle.jsx'));
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
+const Layout = () => (
+	<>
+  <AnimatePresence>
+    
+    <div className="bg-secondary-light dark:bg-primary-dark transition duration-300">
+      <ScrollToTop />
+      <AppHeader />
+      <Suspense fallback={""}>
+        <Outlet />
+      </Suspense>
+      <AppFooter />
+      <UseScrollToTop />
+    </div>
+  </AnimatePresence>
+	</>
+)
+
+
 // 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/helloworld",
+        element: <div>Hello World</div>,
+      },
+      {
+        path: '/page',
+        element: <App />
+      },
+      {
+        path: "/home",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      },
+      {
+        path: "/projects",
+        element: <Projects />
+      },
+      {
+        path: "projects/single-project",
+        element: <ProjectSingle />
+      }
+    ]
   },
   {
-    path: "/helloworld",
-    element: <div>Hello World</div>,
+    path: "/",
+    element: <App />,
   },
 ]);
 
 root.render(
   <React.StrictMode>
-    
     <RouterProvider router={router} />
-
   </React.StrictMode>
 );
 
